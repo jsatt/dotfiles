7dd5cd328b7ab317d463b6a307227f6928f9d391
Jeremy Satterfield <jsatt@jsatt.com>
2022-04-11 18:33:08 +0000

    allow graceful nvim boot if impatient not installed

--- config/nvim/lua/startup_.lua
+++ config/nvim/lua/startup_.lua
@@ -1,2 +1,3 @@
-require('impatient') -- optimize lua plugins
+local utils = require('utils_')
+utils.prepare_module('impatient', utils.noop) -- optimize lua plugins
 vim.opt.termguicolors = vim.fn.has("termguicolors") == 1 and true or false -- enable RGB colors if available
