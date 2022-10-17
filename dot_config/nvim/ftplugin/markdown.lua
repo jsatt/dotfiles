c52c23e7d2630449533585ae994f9548e3998c4c
Jeremy Satterfield <jeremy.satterfield@onepeloton.com>
2022-10-17 14:13:30 +0000

    update vim markdown ftplugin

--- dot_config/nvim/ftplugin/markdown.lua
+++ dot_config/nvim/ftplugin/markdown.lua
@@ -1,3 +1,3 @@
+vim.wo.spell = true
 vim.g.vim_markdown_folding_disabled = 1
-vim.api.nvim_create_autocmd('FileType', {pattern = {'markdown'}, command='setlocal spell spelllang=en_us'})
 vim.api.nvim_buf_set_keymap(0, 'n', '<leader>md', ':%!Markdown.pl --html4tags <cr>', {noremap = true})
