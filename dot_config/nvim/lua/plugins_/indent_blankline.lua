400169d91f14e1c5d0cdb2e7186e251c467b6a9c
Jeremy Satterfield <jeremy.satterfield@onepeloton.com>
2023-10-17 16:03:23 +0000

    update indent_blankline to v3

--- dot_config/nvim/lua/plugins_/indent_blankline.lua
+++ dot_config/nvim/lua/plugins_/indent_blankline.lua
@@ -1,8 +1,5 @@
 return {
   'lukas-reineke/indent-blankline.nvim',
-  opts = {
-    show_current_context = true,
-    show_trailing_blankline_indent = false,
-    use_treesitter_scope = true,
-  },
+  main = 'ibl',
+  opts = {},
 }
