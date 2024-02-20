2c418031f28248b753452f4379983a0013ca6aa9
Jeremy Satterfield <jeremy.satterfield@onepeloton.com>
2024-02-20 21:12:55 +0000

    add history scrollback in nvim telescope windows

--- dot_config/nvim/lua/plugins_/telescope.lua
+++ dot_config/nvim/lua/plugins_/telescope.lua
@@ -8,33 +8,38 @@ return {
     'nvim-telescope/telescope-dap.nvim',
     "debugloop/telescope-undo.nvim",
   },
-  opts = {
-    defaults = {
-      vimgrep_arguments = {
-        'rg',
-        '--color', 'never',
-        '--no-heading',
-        '--with-filename',
-        '--line-number',
-        '--column',
-        '--smart-case',
-        '--hidden',
-        "--glob=!.git/",
-      }
-    },
-    extensions = {
-      undo = {
-        side_by_side = true,
-        layout_strategy = "vertical",
-        layout_config = {
-          preview_height = 0.8,
-        },
-      },
-    },
-  },
   config = function(_, opts)
     local telescope = require('telescope')
-    telescope.setup(opts)
+    telescope.setup({
+      defaults = {
+        mappings = {
+          i = {
+            ["<C-Down>"] = require('telescope.actions').cycle_history_next,
+            ["<C-Up>"] = require('telescope.actions').cycle_history_prev,
+          },
+        },
+        vimgrep_arguments = {
+          'rg',
+          '--color', 'never',
+          '--no-heading',
+          '--with-filename',
+          '--line-number',
+          '--column',
+          '--smart-case',
+          '--hidden',
+          "--glob=!.git/",
+        }
+      },
+      extensions = {
+        undo = {
+          side_by_side = true,
+          layout_strategy = "vertical",
+          layout_config = {
+            preview_height = 0.8,
+          },
+        },
+      },
+    })
     telescope.load_extension('undo')
     utils.prepare_module('aerial', function() telescope.load_extension('aerial') end)
     utils.prepare_module('telescope._extensions.dap', function() telescope.load_extension('dap') end)
