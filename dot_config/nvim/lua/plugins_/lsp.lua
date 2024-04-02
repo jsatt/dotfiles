cb2a61bdc062682b451412c8f0b987b8868040b3
Jeremy Satterfield <jeremy.satterfield@onepeloton.com>
2024-04-02 19:27:41 +0000

    change to basedpyright and drop deprecated none-lsp plugins

--- dot_config/nvim/lua/plugins_/lsp.lua
+++ dot_config/nvim/lua/plugins_/lsp.lua
@@ -35,7 +35,7 @@ local lsp_configs = {
       }
     },
   },
-  pyright = {
+  basedpyright = {
     settings = {
       python = {
         analysis = {
@@ -99,7 +99,6 @@ local dap_configs = {
   'bash-debug-adapter',
 }
 local linter_configs = {
-  'cspell',
 }
 local formatter_configs = {
   'prettierd',
@@ -168,10 +167,8 @@ return {
         local null_ls = require('null-ls')
         null_ls.setup({
           sources = {
-            null_ls.builtins.code_actions.cspell,
             null_ls.builtins.code_actions.gitrebase,
             null_ls.builtins.code_actions.gitsigns,
-            null_ls.builtins.diagnostics.cspell.with({ extra_filetypes = { 'text', 'markdown', 'gitcommit' } }),
             null_ls.builtins.diagnostics.zsh,
 
             null_ls.builtins.formatting.isort,
