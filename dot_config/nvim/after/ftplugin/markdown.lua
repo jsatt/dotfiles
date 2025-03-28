0117950c0b0854e68b279b729dcefd4ec28ea5b2
Jeremy Satterfield <jeremy.satterfield@onepeloton.com>
2025-03-28 13:37:09 -0500

    update ft specific neovim configs

--- dot_config/nvim/after/ftplugin/markdown.lua
+++ dot_config/nvim/after/ftplugin/markdown.lua
@@ -1,3 +1,6 @@
-vim.wo.spell = true
+vim.bo.shiftwidth = 2 -- 2 spaces for indents when using << or >>
+vim.bo.softtabstop = 2 -- 2 spaces for tabs in INSERT mode
+vim.bo.tabstop = 2 -- 2 space for tabs
+
 vim.g.vim_markdown_folding_disabled = 1
 vim.api.nvim_buf_set_keymap(0, 'n', '<leader>md', ':%!Markdown.pl --html4tags <cr>', {noremap = true})
