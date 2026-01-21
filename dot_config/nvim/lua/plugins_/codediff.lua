1121236c4ff6ab81863eaeb8ac4d37007ebcf512
Jeremy Satterfield <jeremy.satterfield@onepeloton.com>
2026-01-21 15:35:56 -0600

    add nvim codediff and make default fot git merge and diffs

---
+++ dot_config/nvim/lua/plugins_/codediff.lua
@@ -0,0 +1,13 @@
+return {
+  "esmuellert/codediff.nvim",
+  dependencies = { "MunifTanjim/nui.nvim" },
+  cmd = "CodeDiff",
+  opts = {
+    explorer = {
+      view_mode = 'tree',
+    },
+    history = {
+      view_mode = 'tree',
+    },
+  },
+}
