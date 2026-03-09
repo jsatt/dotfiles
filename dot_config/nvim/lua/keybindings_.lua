db41627261d0a6599916bb4973d2d06613fa6173
Jeremy Satterfield <jsatt@jsatt.com>
2026-03-09 13:33:54 -0500

    handle nvim wrapped line navigation bindings in visual mode

--- dot_config/nvim/lua/keybindings_.lua
+++ dot_config/nvim/lua/keybindings_.lua
@@ -28,10 +28,12 @@ vim.keymap.set('n', 'L', '$', { desc = 'Goto end of line' })
 vim.keymap.set('v', 'L', 'g_', { desc = 'Goto end of line' })
 
 -- navigation on wrapped lines
-vim.keymap.set('n', 'k', 'v:count == 0 ? "gk": "k"', { expr = true })
-vim.keymap.set('n', 'j', 'v:count == 0 ? "gj": "j"', { expr = true })
-vim.keymap.set('n', '<Up>', 'v:count == 0 ? "gk": "k"', { expr = true })
-vim.keymap.set('n', '<Down>', 'v:count == 0 ? "gj": "j"', { expr = true })
+vim.keymap.set({'n', 'v'}, 'k', 'v:count == 0 ? "gk": "k"', { expr = true })
+vim.keymap.set({'n', 'v'}, 'j', 'v:count == 0 ? "gj": "j"', { expr = true })
+vim.keymap.set({'n', 'v'}, '<Up>', 'v:count == 0 ? "gk": "k"', { expr = true })
+vim.keymap.set({'n', 'v'}, '<Down>', 'v:count == 0 ? "gj": "j"', { expr = true })
+vim.keymap.set('i', '<Up>', 'v:count == 0 ? "<C-o>gk": "<C-o>k"', { expr = true })
+vim.keymap.set('i', '<Down>', 'v:count == 0 ? "<C-o>gj": "<C-o>j"', { expr = true })
 
 -- navigation between tabs
 vim.keymap.set('n', '<C-Right>', ':tabnext<CR>', { silent = true, desc = 'Navigate tabs - right' })
